9bafe910783e69203ef6ff344f7919c422f32659
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-05 17:12:34 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const neuerSocialMediaPost = document.createElement("article");
+neuerSocialMediaPost.classList.add("post");
+
+const neuerText = document.createElement("p");
+neuerText.classList.add("post__content");
+neuerText.textContent = "Hallo, ich bin ein neuer SocialMediaPost";
+neuerSocialMediaPost.append(neuerText);
+
+const neuerUserName = document.createElement("footer");
+neuerUserName.classList.add("post__username");
+neuerUserName.textContent = "@Nadine";
+neuerSocialMediaPost.append(neuerUserName);
+
+const neuerButton = document.createElement("button");
+neuerButton.classList.add("post__button");
+neuerButton.textContent = "  ♥ Like";
+neuerSocialMediaPost.append(neuerButton);
+
+document.body.append(neuerSocialMediaPost); // elternElement.append(kindElement)
+
+neuerButton.addEventListener("click", handleLikeButtonClick);
+
+console.log(neuerSocialMediaPost);
